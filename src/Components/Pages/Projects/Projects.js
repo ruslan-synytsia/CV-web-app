1863b2f1cd1440e905e47e717283bd74d8db778f
Ruslan <64065765+ruslan-synytsia@users.noreply.github.com>
2023-10-31 10:53:48 +0100

    Update Projects.js

--- src/Components/Pages/Projects/Projects.js
+++ src/Components/Pages/Projects/Projects.js
@@ -6,13 +6,13 @@ function Projects() {
         {
             header: "Chat Application",
             image: "./chat.png",
-            description: "The Simple Fullstack (MERN) application./The application is written in three parts: client, authentication server (REST) and chat server (WebSockets)./First, the authentication process occurs - recording a new user in the database and sending the server cookies (HttpOnly) with access tokens in two options: accessToken&refreshToken or only accessToken to the client, depending on the state of the AutoLogin component (true or false)./Access to the protected chat resource remains open only as long as the Access Token is valid./The interaction between the client and the chat server is carried out using the socket.io library on the secure home page./The chat server application implements the ability to exchange messages, like between all chat participants in Public Chat, and private chat rooms./In this FullStack application, the basic functionality is intentionally implemented in order to demonstrate the implementation, the above technologies and libraries.",
+            description: "The Simple Fullstack (MERN) application.|The application is written in three parts: client, authentication server (REST) and chat server (WebSockets).|First, the authentication process occurs - recording a new user in the database and sending the server cookies (HttpOnly) with access tokens in two options: accessToken&refreshToken or only accessToken to the client, depending on the state of the AutoLogin component (true or false).|Access to the protected chat resource remains open only as long as the Access Token is valid.|The interaction between the client and the chat server is carried out using the socket.io library on the secure home page.|The chat server application implements the ability to exchange messages, like between all chat participants in Public Chat, and private chat rooms.|In this FullStack application, the basic functionality is intentionally implemented in order to demonstrate the implementation, the above technologies and libraries.",
             link: "https://corporate-chat-app.vercel.app/"
         },
         {
             header: "Weather Application",
             image: "./weather.png",
-            description: "This Single Page Application was created using React.",
+            description: "A simple React application.|Redux is used as a state manager (I used the @reduxjs/toolkit package to work with the manager).|The source of external data displayed on the client is Weather API from https://openweathermap.org/.|The application consists of three screens:|1 - locale initialization page, where you need to enter the City.|2 - the main page of the application with all the main elements with data.|3 - additional page displaying a chart for a period of 5 days.|Design ideas were taken from open sources.",
             link: "https://weather-app-f36y.onrender.com/"
         },
         {
@@ -47,8 +47,8 @@ function Projects() {
         }
     ];
 
-    const stringToList = (str, tag) => {
-    const arr = str.split('/');
+    const stringToList = (str, tag = 'span') => {
+    const arr = str.split('|');
         if (tag === 'li') {
             return arr.map((paragraph, index) => <li key={index}>{paragraph}</li>)
         } else {
