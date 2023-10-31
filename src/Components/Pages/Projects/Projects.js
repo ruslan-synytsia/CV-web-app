d713a5618697402583b32dad59f0cf37536cb72e
Ruslan <64065765+ruslan-synytsia@users.noreply.github.com>
2023-10-31 08:45:20 +0100

    Update Projects.js
    Updated projects array: added new project

--- src/Components/Pages/Projects/Projects.js
+++ src/Components/Pages/Projects/Projects.js
@@ -4,15 +4,21 @@ import style from './Projects.module.css';
 function Projects() {
     const projects = [
         {
+            header: "Chat Application",
+            image: "./chat.png",
+            description: "This simple Fullstack application was created based on MERN.",
+            link: "https://corporate-chat-app.vercel.app/"
+        },
+        {
             header: "Weather Application",
             image: "./weather.png",
-            description: "This is Single Page Application was created using React.",
+            description: "This Single Page Application was created using React.",
             link: "https://weather-app-f36y.onrender.com/"
         },
         {
             header: "Find Movies Application",
             image: "./project.png",
-            description: "This is Single Page Application was created using React.",
+            description: "This Single Page Application was created using React.",
             link: "https://symphonious-paprenjak-fac495.netlify.app/"
         },
         {
@@ -24,19 +30,19 @@ function Projects() {
         {
             header: "Bouncy Template",
             image: "./project-2.png",
-            description: "This html template was created using HTML, CSS and JS.",
+            description: "This html template was created using HTML&CSS and JS.",
             link: "http://resume-ruslan-synytsia.atwebpages.com/bouncy-template/"
         },
         {
             header: "JS Game Application",
             image: "./project-3.png",
-            description: "This JS application was created using HTML, CSS and JS Canvas.",
+            description: "This JS application was created using HTML&CSS and JS Canvas.",
             link: "http://resume-ruslan-synytsia.atwebpages.com/hangman-game/"
         },
         {
             header: "JS Calculator",
             image: "./project-4.png",
-            description: "This html template was created using HTML, CSS and JS.",
+            description: "This html template was created using HTML&CSS and JS.",
             link: "http://resume-ruslan-synytsia.atwebpages.com/calculator/"
         }
     ];
